357eac27dc37e080821e5911a552a6471a876c7f
joshuarte <joshuarte@users.noreply.github.com>
2017-06-19 18:05:01 +0200

    ripush
    ripush

--- app/views/dashboard/dashboard.js
+++ app/views/dashboard/dashboard.js
@@ -6,9 +6,10 @@ angular.module('app.dashboard', [])
     var vm = this;
     vm.componentName = "Dashboard";
     /*Imposto la variabile elenco alla risposta del servizio*/
-    vm.elenco = elencoCitta.elenco
-
+    vm.elenco = elencoCitta.elenco;
+    vm.citta = {};
     vm.aggiungiCitta = function(){
+      console.log(vm.citta)
       elencoCitta.aggiungi({
         nome: vm.citta.nome, 
         regione: vm.citta.regione
