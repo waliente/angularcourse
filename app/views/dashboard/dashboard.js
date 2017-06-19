474102c124bbbc2f88bb67bf232ea88d88fe9674
joshuarte <joshuarte@users.noreply.github.com>
2017-06-19 18:01:32 +0200

    Aggiungi citta test

--- app/views/dashboard/dashboard.js
+++ app/views/dashboard/dashboard.js
@@ -5,6 +5,13 @@ angular.module('app.dashboard', [])
   .controller('dashboardCtrl', ['$scope', 'elencoCitta' , function ($scope, elencoCitta) {
     var vm = this;
     vm.componentName = "Dashboard";
-
+    /*Imposto la variabile elenco alla risposta del servizio*/
     vm.elenco = elencoCitta.elenco
+
+    vm.aggiungiCitta = function(){
+      elencoCitta.aggiungi({
+        nome: vm.citta.nome, 
+        regione: vm.citta.regione
+      })
+    }
   }]);
