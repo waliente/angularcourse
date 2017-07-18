77c16ad11ae5ef7df0bf60a0f5f006d9cd7cf8e6
Luca Joshua Mosca <joshuarte@hotmail.it>
2017-07-18 16:36:59 +0200

    prova icons sides

--- app/shared/components/cardList/cardList.js
+++ app/shared/components/cardList/cardList.js
@@ -13,11 +13,42 @@ angular.module("app.cardListComponent", [])
     .controller('cardListCtrl', ['$scope', 'elencoCarte', function ($scope, elencoCarte) {
         var vm = this;
 
+        var diceIcons = {
+            ranged: 'RD',
+            melee: 'MD',
+            focus: 'F',
+            discard: 'Dc',
+            disrupt: 'Dr',
+            resource: 'R',
+            special: 'Sp',
+            blank: '-'
+        };
+
+
+
 
         elencoCarte.getAll()
             /*Successo (success)*/
             .then(function (res) {
                 console.log(res);
+
+                $scope.filtraSides = function (data) {
+
+                    angular.forEach(diceIcons, function (value, key) {
+                        // console.log(data, value, key);
+                        if(data.includes(value)){
+                            var newData;
+                            var icon = document.createElement('span');
+                            icon.classList.add(`icon`, `icon-${key}`);
+
+                            newData = data.replace(value, icon);
+                            console.log(newData);
+                        }
+                        
+                        return newData;
+                    });
+                };
+
                 vm.elencoCarte = res.data;
             })
             /*Errore (error)*/
